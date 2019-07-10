f2fcfb677f117a47c13301257c9a97652c266ecd
Jerry Pan <jerry@IT-18.local>
2019-07-10 10:09:38 +0800

    major update to schema; code

--- index.js
+++ index.js
@@ -1,85 +1,67 @@
 /* eslint no-console: ["error", { allow: ["log"] }] */
 require('dotenv').config();
 const AWS = require('aws-sdk');
-const Joi = require('@hapi/joi');
 
 AWS.config.update({ region: 'ap-southeast-1' });
 
 class RDPLog {
   constructor() {
     this.firehose = new AWS.Firehose();
-    this.recordSchema = Joi.object().keys({
-      flag: Joi.string().valid(['log', 'debug', 'info', 'warn', 'error']).required(),
-      message: Joi.string().required(),
-      createdAt: Joi.date().required(),
-    });
-    this.saveLog = (record) => {
-      Joi.validate(record, this.recordSchema, (validationErr, value) => {
-        if (validationErr) {
-          console.log({
-            status: 'validation error',
-            message: 'validationErr.details',
-          });
-        } else {
-          this.firehose.putRecord(
-            {
-              DeliveryStreamName: process.env.DELIVERY_STREAM_NAME_STAG,
-              Record: {
-                Data: Buffer.from(JSON.stringify(value)),
-              },
+    this.transformRecord = (flag, record) => {
+      const transformedRecord = {};
+      transformedRecord.createdAt = (new Date()).toUTCString();
+      transformedRecord.flag = flag;
+      if(record.product) {
+        transformedRecord.product = record.product;
+      } else {
+        throw "product field cannot be empty or omitted";
+      }
+      transformedRecord.user =  record.user ? record.user : "root";
+      transformedRecord.message = record.message;
+      return transformedRecord;
+    };
+    this.saveLog = async (record) => {
+      try {
+        const promise = await this.firehose.putRecord(
+          {
+            DeliveryStreamName: process.env.DELIVERY_STREAM_NAME_STAG,
+            Record: {
+              Data: Buffer.from(JSON.stringify(record, null, 2)),
             },
-            (err, data) => {
-              if (err) {
-                console.log({
-                  status: 'log failure',
-                  message: err.stack,
-                });
-              } else {
-                console.log({
-                  status: 'log success',
-                  recordId: data.RecordId,
-                });
-              }
-            },
-          );
+          },
+        ).promise();
+        if(process.env.ENVIRONMENT === 'dev' && promise){
+          console.log(JSON.stringify(record, null, 2));
         }
-      });
+      } catch(err) {
+        console.log('error', err);
+      }
     };
   }
 
-  log(logMessageObj) {
-    const record = logMessageObj;
-    record.flag = 'log';
-    record.createdAt = new Date();
-    this.saveLog(record);
+  async log(logMessageObj) {
+    const record = this.transformRecord("log", logMessageObj);
+    await this.saveLog(record);
   }
 
-  info(logMessageObj) {
-    const record = logMessageObj;
-    record.flag = 'info';
-    record.createdAt = new Date();
-    this.saveLog(record);
+  async info(logMessageObj) {
+    const record = this.transformRecord("info", logMessageObj);
+    await this.saveLog(record);
   }
 
-  debug(logMessageObj) {
-    const record = logMessageObj;
-    record.flag = 'debug';
-    record.createdAt = new Date();
-    this.saveLog(record);
+  async debug(logMessageObj) {
+    const record = this.transformRecord("debug", logMessageObj);
+    await this.saveLog(record);
   }
 
-  warn(logMessageObj) {
-    const record = logMessageObj;
-    record.flag = 'warn';
-    record.createdAt = new Date();
-    this.saveLog(record);
+  async warn(logMessageObj) {
+    const record = this.transformRecord("warn", logMessageObj);
+    await this.saveLog(record);
   }
 
-  error(logMessageObj) {
-    const record = logMessageObj;
-    record.flag = 'error';
-    record.createdAt = new Date();
-    this.saveLog(record);
+  async error(logMessageObj) {
+    const record = this.transformRecord("error", logMessageObj);
+    await this.saveLog(record);
   }
 }
 
