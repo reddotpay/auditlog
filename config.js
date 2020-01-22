aa1addc8667cd1bf98581727f3c3e0cd0b5b3b8c
jerrypan <jerry@IT-18.local>
2020-01-22 13:33:23 +0800

    updated env variable

--- config.js
+++ config.js
@@ -3,4 +3,5 @@ require('dotenv').config();
 module.exports = {
   environment: process.env.ENVIRONMENT,
   deliveryStreamName: process.env.DELIVERY_STREAM_NAME,
+  displayAuditlog: process.env.DISPLAY_AUDITLOG,
 };
