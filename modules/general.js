df5fc055f3b47b271a0d753add4f16fcb0cbe42e
jerrypan <jerry@IT-18.local>
2020-01-23 09:51:33 +0800

    refactor(general): added eslint - PE-909

--- modules/general.js
+++ modules/general.js
@@ -1,8 +1,8 @@
 module.exports = {
   convertToString: (data) => {
     let cache = [];
-  
-    const res = JSON.stringify(data, function(key, value) {
+
+    const res = JSON.stringify(data, (key, value) => {
       if (typeof value === 'object' && value !== null) {
         if (cache.indexOf(value) !== -1) {
           return;
@@ -10,10 +10,10 @@ module.exports = {
         cache.push(value);
       }
       return value;
-    });
-  
+    }, 2);
+
     cache = null;
-  
+
     return res;
   },
   getStackTrace: () => {
@@ -32,15 +32,15 @@ module.exports = {
     transformedRecord.createdAt = (new Date()).toUTCString();
     transformedRecord.flag = flag;
     transformedRecord.summary = summary;
-    if(product && product !== null && product !== "") {
+    if (product && product !== null && product !== '') {
       transformedRecord.product = product;
     }
-    if(user && user !== null && user !== "") {
+    if (user && user !== null && user !== '') {
       transformedRecord.user = user;
     } else {
-      transformedRecord.user = "root";
+      transformedRecord.user = 'root';
     }
     transformedRecord.message = [...message];
     return transformedRecord;
   },
-}
+};
